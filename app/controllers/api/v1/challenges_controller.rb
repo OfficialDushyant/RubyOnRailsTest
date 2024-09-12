f9115ccbf4e55de2df40250b4f45365284f2ebcb
Dushyant Patel <devops@bydushyant.com>
2024-09-11 21:50:23 -0400

    Optimize according to conventions

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ChallengesController < ApplicationController
+      before_action :set_challenge, only: %i[show update destroy]
       # GET /api/v1/challenges
       def index
         @challenges = Challenge.all
@@ -8,39 +9,39 @@ module Api
       end
       # POST  /api/v1/challenges
       def create
-        challenge = Challenge.new(challenge_params)
-        if challenge.save
-          render json: { message: "Challenge added successfully", data: challenge }
+        @NewChallenge = Challenge.new(challenge_params)
+        if @NewChallenge.save
+          render json: { message: "Challenge added successfully", data: @NewChallenge }
         else
-            render status: :bad_request, json: { message: "Challenge not added", data: challenge.errors }
+            render status: :bad_request, json: { message: "Challenge not added", data: @challenge.errors }
         end
       end
       # GET /api/v1/challenges/:id
       def show
-        challenge =Challenge.find(params[:id])
-        if challenge
-          render json: { message: "Challenge fond", data: challenge }
+        if @challenge
+          render json: { message: "Challenge fond", data: @challenge }
         else
-          render status: :not_found, json: { message: "Challenge not found" }
+          render status: :not_found, json: { message: @challenge.errors }
         end
       end
       # PATCH | PUT /api/v1/challenges/:id
       def update
-        challenge = Challenge.find(params[:id])
-        if challenge.update(challenge_params)
-          render json: { message: "Challenge updated successfully", data: challenge }
+        if @challenge.update(challenge_params)
+          render json: { message: "Challenge updated successfully", data: @challenge }
         else
-            render status: :unprocessable_entity, json: { message: "Challenge not updated" }
+            render status: :unprocessable_entity, json: { message: @challenge.errors }
         end
       end
       # DELETE /api/v1/challenges/:id
       def destroy
-        challenge = Challenge.find(params[:id])
-        challenge.destroy
+        @challenge.destroy
         head :no_content
       end
 
       private
+      def set_challenge
+        @challenge = Challenge.find(params[:id])
+      end
       def challenge_params
         params.require(:challenge).permit(:title, :description, :start_date, :end_date)
       end
